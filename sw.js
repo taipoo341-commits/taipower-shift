95811a77fa172d63c119ac2649f4f451cafa9e6b
taipoo341-commits <taipoo341@gmail.com>
2026-05-14 18:10:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('fetch', function(event) {
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function (event) {
   event.respondWith(fetch(event.request));
 });
